f9a45dfe5483bf6fe9fbf1e71ba593b89817d942
Aaron <aaronknobloch92@gmail.com>
2017-02-03 12:06:17 -0500

    Hides acceptance/difficulty on /problemset

--- injection.js
+++ injection.js
@@ -1,7 +1,16 @@
+var acceptanceHeader = document.getElementsByClassName("reactable-th-acceptance reactable-header-sortable reactable-header-sort-desc")[0];
+acceptanceHeader.style.display = "none";
+
+var difficultyHeader = document.getElementsByClassName("reactable-th-difficulty reactable-header-sortable ")[0];
+difficultyHeader.style.display = "none";
+
 var problemTable = document.getElementsByClassName("reactable-data")[0];
 var rows = problemTable.rows;
 
 for(var i = 0; i < rows.length; i++)
 {
-	rows[i].cells[5].innerHTML = "Show";
+	
+	rows[i].cells[4].style.display = "none";
+	rows[i].cells[5].style.display = "none";
 }
+
