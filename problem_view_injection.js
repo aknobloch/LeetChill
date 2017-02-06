93b210ba22169f1f4d7e92afb2cb15b93a19fcc8
Aaron <aaronknobloch92@gmail.com>
2017-02-06 14:41:39 -0500

    Adds toggle button to problem view

--- problem_view_injection.js
+++ problem_view_injection.js
@@ -1,9 +1,53 @@
-var infoList = document.getElementsByClassName("question-info text-info")[0];
-var items = infoList.getElementsByTagName("li");
+// constant to track hide/show toggle
+var isHidden = false;
 
-// hide the first three list items (accepted, submitted and difficulty)
-for(var i = 0; i < 3; i++)
+function setVisibility()
 {
-	var listItem = items[i];
-	listItem.style.visibility = "hidden";
+	// defines what to set the current state as
+	var state = "hidden";
+	
+	if(isHidden)
+	{
+		state = "visible";
+	}
+	
+	var infoList = document.getElementsByClassName("question-info text-info")[0];
+	var items = infoList.getElementsByTagName("li");
+
+	// hide the first three list items (accepted, submitted and difficulty)
+	for(var i = 0; i < 3; i++)
+	{
+		var listItem = items[i];
+		listItem.style.visibility = state;
+	}
+	
+	// toggle for next time
+	isHidden = ! isHidden;
 }
+
+function addButton()
+{
+	// create button
+	var btn = document.createElement("BUTTON");
+	var btnTxt = document.createTextNode("Toggle LeetChill");
+	btn.appendChild(btnTxt);
+
+	// pretify it
+	btn.className = "btn btn-success btn-md btn-action";
+
+	// add functionality
+	btn.onclick = function() 
+	{
+		setVisibility();
+	};
+
+	// add btn to appropriate location
+	var infoList = document.getElementsByClassName("question-info text-info")[0];
+	infoList.appendChild(btn);
+	btn.style.marginLeft = (infoList.offsetWidth / 2) - (btn.offsetWidth / 2) + "px";
+	btn.style.marginTop = "10px";
+}
+
+
+addButton();
+setVisibility();
